35b442fc56eb29985e61aca81c0a14b1af3ff322
alexarechigam98 <156549699+alexarechigam98@users.noreply.github.com>
2025-08-20 12:22:42 -0600

    Add files via upload

---
+++ Compuerta_AND/_and.v
@@ -0,0 +1,11 @@
+
+//1.Definicion del modulo y sus I/O
+//Dentro del parentesis se define los I/O
+module _and(input a, input b, output c);
+//2.Definen cables o componenetes internos
+//NA
+//3. Asignaciones, instancias, conexiones
+
+assign c = a & b;
+
+endmodule
